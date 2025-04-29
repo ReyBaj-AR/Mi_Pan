67f7f5e12f9da2c05f70e60620d96e30de5499f0
ReyBaj-AR <javyvarela@gmail.com>
2025-04-29 14:47:07 -0300

    Agregué mi archivo Main.py

---
+++ Main.py
@@ -0,0 +1,23 @@
+print("¡Hola, Panaderos!\n")
+print("Esta es una calculadora de porcentaje panadero que calcula en Gramos a partir de la harina")
+print("Ingresá la cantidad de harina que tenes para hacer pan, y el sistema te devuelve las cantidades para el resto de los ingredientes\n")
+print ("Pan de Molde Clásico\n")
+
+harina = 850 # Harina representa el 100%
+
+agua = harina * 0.6  # Agua es el 60% de la harina
+manteca = harina * 0.15  # Manteca es el 15% de la harina
+sal = harina * 0.02  # Sal es el 2% de la harina
+miel = harina * 0.03 # La miel es el 3% de la harina
+azucar = harina * 0.02 # El azucar es el 2% de la harina
+
+
+print(f"Harina: {harina} Gramos\n")
+print(f"Agua: {agua} Gramos")
+print(f"Manteca: {manteca} Gramos")
+print(f"Sal: {sal} Gramos")
+print (f"Miel: {miel} Gramos")
+print (f"Azucar: {azucar} Gramos\n")
+print ("Que te salga rico el pan - Un Abrazo, ReyBaj.\n\n")
+
+
