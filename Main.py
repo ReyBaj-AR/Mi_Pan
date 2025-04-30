b538cedf61e9deeca8cb353e370fed05a157f111
ReyBaj-AR <javyvarela@gmail.com>
2025-04-30 17:09:32 -0300

    Incluye Hokkaido

--- Main.py
+++ Main.py
@@ -3,21 +3,52 @@ print("Esta es una calculadora de porcentaje panadero que calcula en Gramos a pa
 print("Ingresá la cantidad de harina que tenes para hacer pan, y el sistema te devuelve las cantidades para el resto de los ingredientes\n")
 print ("Pan de Molde Clásico\n")
 
-harina = 850 # Harina representa el 100%
+harina = 17000 # Harina representa el 100%
 
 agua = harina * 0.6  # Agua es el 60% de la harina
 manteca = harina * 0.15  # Manteca es el 15% de la harina
 sal = harina * 0.02  # Sal es el 2% de la harina
 miel = harina * 0.03 # La miel es el 3% de la harina
 azucar = harina * 0.02 # El azucar es el 2% de la harina
+levadura = harina * 0.01 # La levadura es el 2% de la harina
+
 
 
 print(f"Harina: {harina} Gramos\n")
 print(f"Agua: {agua} Gramos")
 print(f"Manteca: {manteca} Gramos")
 print(f"Sal: {sal} Gramos")
-print (f"Miel: {miel} Gramos")
-print (f"Azucar: {azucar} Gramos\n")
+print(f"Miel: {miel} Gramos")
+print (f"Azucar: {azucar} Gramos")
+print(f"Levadura: {levadura} Gramos\n")
 print ("Que te salga rico el pan - Un Abrazo, ReyBaj.\n\n")
 
+
+print("Pan de Hokkaido\n")
+
+harina_inicial = 8000
+
+tangzhong_harina_inicial = harina *0.065  #Es el 6.5% de la harina inicial
+tangzhong_agua = harina_inicial *0.32  #Es el 32% de la harina inicial
+harina_para_pan = harina_inicial * 0.93 # Es la harina que queda luego de sacar para el Tanghzon
+azucar = harina_inicial * 0.1 # Es el 10% de la harina inicial
+sal= harina_inicial * 0.02 # Es el 2% de la harina inicial
+levadura_seca = harina_inicial * 0.015 # Es el 1.5% de la harina inicial 
+leche_fluida = harina_inicial * 0.40 # Es el 40% de la harina inicial
+huevo = harina_inicial * 0.08 # Es el 8% de la harina inicial
+manteca = harina_inicial *0.08 # Es el 8% de la harina inicial
+
+
+print(f"Harina Inicial {harina_inicial} Gramos\n")
+print(f"Tangzong Harina: {tangzhong_harina_inicial} Gramos")
+print(f"Tangzong Agua: {tangzhong_agua} Gramos\n")
+print(f"Harina para pan: {harina_para_pan} Gramos")
+print(f"Azucar: {azucar} Gramos")
+print(f"Sal: {sal} Gramos")
+print(f"Levadura Seca: {levadura_seca} Gramos")
+print(f"Leche Fluida: {leche_fluida} Gramos")
+print(f"Huevo: {huevo} Gramos")
+print(f"Manteca: {manteca} Gramos\n\n")
+
+print ("Que te salga rico el pan - Un Abrazo, ReyBaj.\n\n")
 
